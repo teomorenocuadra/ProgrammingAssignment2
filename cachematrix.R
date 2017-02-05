3a4b6dc952abcfe5f5af084bab64801434fa426a
teomorenocuadra <teomorenocuadra@gmail.com>
2017-02-05 12:58:56 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a
+## special "matrix" object that can cache its inverse.
 
 ## Write a short comment describing this function
 
