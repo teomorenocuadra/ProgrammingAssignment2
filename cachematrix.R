9731861573fd8ecb74e65716ffa1a95460bce383
teomorenocuadra <teomorenocuadra@gmail.com>
2017-02-05 13:01:28 -0500

    First function code.

--- cachematrix.R
+++ cachematrix.R
@@ -12,8 +12,18 @@
 ## environment different from the current one.
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}  
 
 
 ## Write a short comment describing this function
